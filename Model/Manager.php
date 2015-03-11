40b99f61bb14fd8bf22a5e82fb417ca3ad04ba92
Nathanael d. Noblet <nathanael@noblet.ca>
2015-03-11 16:54:25 -0600

    new variable names and docblocks

--- Model/Manager.php
+++ Model/Manager.php
@@ -9,100 +9,172 @@ use \Doctrine\Common\Persistence\ObjectManager;
 
 class Manager implements ObjectManager
 {
-    private $_em;
-    private $_securityContext;
-    private $_securedQuery;
-    
-    public function __construct(SecurityContextInterface $securityContext, ObjectManager $em, SecuredQuery $sqb)
+    private $entityManager;
+
+    private $securityContext;
+
+    private $securedQuery;
+
+    /**
+     *
+     * @param SecurityContextInterface $securityContext
+     * @param ObjectManager $entityMgr
+     * @param SecuredQuery $securedQuery
+     * @return \NS\SecurityBundle\Model\Manager
+     */
+    public function __construct(SecurityContextInterface $securityContext, ObjectManager $entityMgr, SecuredQuery $securedQuery)
     {
-        $this->_em              = $em;
-        $this->_securityContext = $securityContext;
-        $this->_securedQuery    = $sqb;
-        
+        $this->entityManager   = $entityMgr;
+        $this->securityContext = $securityContext;
+        $this->securedQuery    = $securedQuery;
+
         return $this;
     }
-    
+
+    /**
+     *
+     * @return ObjectManager
+     */
     public function getEntityManager()
     {
-        return $this->_em;
+        return $this->entityManager;
     }
-    
+
+    /**
+     *
+     * @return SecurityContextInterface
+     */
     public function getSecurityContext()
     {
-        return $this->_securityContext;
+        return $this->securityContext;
     }
-    
+
+    /**
+     * {@inheritdoc}
+     */
     public function getRepository($class)
     {
-        $repo = $this->_em->getRepository($class);
+        $repo = $this->entityManager->getRepository($class);
         if ($repo && $repo instanceof SecuredRepositoryInterface)
         {
-            $repo->setSecurityContext($this->_securityContext);
-            $repo->setSecuredQuery($this->_securedQuery);
+            $repo->setSecurityContext($this->securityContext);
+            $repo->setSecuredQuery($this->securedQuery);
             $repo->setManager($this);
         }
 
         return $repo;
     }
-    
+
+    /**
+     * {@inheritdoc}
+     */
     public function persist($object)
     {
-        return $this->_em->persist($object);
+        return $this->entityManager->persist($object);
     }
-    
+
+    /**
+     * {@inheritdoc}
+     */
     public function flush()
     {
-        return $this->_em->flush();
+        return $this->entityManager->flush();
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function remove($object)
     {
-        return $this->_em->remove($object);
+        return $this->entityManager->remove($object);
     }
 
-    public function clear($objectName = null) {
-        return $this->_em->clear($objectName);
+    /**
+     * {@inheritdoc}
+     */
+    public function clear($objectName = null)
+    {
+        return $this->entityManager->clear($objectName);
     }
 
-    public function contains($object) {
-        return $this->_em->contains($object);
+    /**
+     * {@inheritdoc}
+     */
+    public function contains($object)
+    {
+        return $this->entityManager->contains($object);
     }
 
-    public function detach($object) {
-        return $this->_em->detach($object);
+    /**
+     * {@inheritdoc}
+     */
+    public function detach($object)
+    {
+        return $this->entityManager->detach($object);
     }
 
-    public function find($className, $id) {
-        return $this->_em->find($className, $id);
+    /**
+     * {@inheritdoc}
+     */
+    public function find($className, $id)
+    {
+        return $this->entityManager->find($className, $id);
     }
 
-    public function getClassMetadata($className) {
-        return $this->_em->getClassMetadata($className);
+    /**
+     * {@inheritdoc}
+     */
+    public function getClassMetadata($className)
+    {
+        return $this->entityManager->getClassMetadata($className);
     }
 
-    public function getMetadataFactory() {
-        return $this->_em->getMetadataFactory();
+    /**
+     * {@inheritdoc}
+     */
+    public function getMetadataFactory()
+    {
+        return $this->entityManager->getMetadataFactory();
     }
 
-    public function initializeObject($obj) {
-        return $this->_em->initializeObject($obj);
+    /**
+     * {@inheritdoc}
+     */
+    public function initializeObject($obj)
+    {
+        return $this->entityManager->initializeObject($obj);
     }
 
-    public function merge($object) {
-        return $this->_em->merge($object);
+    /**
+     * {@inheritdoc}
+     */
+    public function merge($object)
+    {
+        return $this->entityManager->merge($object);
     }
 
-    public function refresh($object) {
-        return $this->_em->refresh($object);
+    /**
+     * {@inheritdoc}
+     */
+    public function refresh($object)
+    {
+        return $this->entityManager->refresh($object);
     }
 
-    public function getUnitOfWork() {
-        return $this->_em->getUnitOfWork();
+    /**
+     * {@inheritdoc}
+     */
+    public function getUnitOfWork()
+    {
+        return $this->entityManager->getUnitOfWork();
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function __call($name, $arguments)
     {
-        if(method_exists($this->_em, $name))
-            return call_user_func_array (array($this->_em,$name), $arguments);
+        if (method_exists($this->entityManager, $name))
+            return call_user_func_array(array($this->entityManager, $name), $arguments);
     }
 }
